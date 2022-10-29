2ce9f27a20c077d811678cc149f136f7107635e6
ValmirTucci <valmirizzo@gmail.com>
2022-10-29 14:17:17 -0300

    Criando a tabela Author, Category, Carrer e Course

--- criandoBanco.sql
+++ criandoBanco.sql
@@ -1,1 +1,80 @@
-CREATE DATABASE [balta]
+CREATE DATABASE [valmir]
+
+CREATE TABLE [Studant](
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(120) NOT NULL,
+    [Email] NVARCHAR(180) NOT NULL,
+    [Document] NVARCHAR(20) NULL,
+    [Phone] NVARCHAR(20) NULL,
+    [Birthdate] DATETIME NULL,
+    [CreatDate] DATETIME NOT NULL DEFAULT (GETDATE()),
+    CONSTRAINT [Pk_Student] PRIMARY KEY ([iD])
+);
+GO
+
+CREATE TABLE [Author] 
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Title] NVARCHAR(80) NOT NULL,
+    [Image] NVARCHAR(1024) NOT NULL,
+    [Bio] NVARCHAR (2000) NOT NULL,
+    [Url] NVARCHAR(450) NULL,
+    [Email] NVARCHAR(160) NOT NULL,
+    [Type] TINYINT NOT NULL, --0 A 255
+    CONSTRAINT [PK_Author] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Career] 
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [Active] BIT NULL,
+    [Featured] BIT NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL, --0 A 255
+    CONSTRAINT [PK_Career] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Category] 
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Sumary] NVARCHAR(2000) NOT NULL,
+    [Order] INT NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Featured] BIT NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Course]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Tag] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR (1024) NOT NULL,
+    [Level] TINYINT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [CreateDate] DATETIME NOT NULL, 
+    [LastUpdateDate] DATETIME NOT NULL,
+    [Active] BIT NOT NULL,
+    [Free] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [AuthorId] UNIQUEIDENTIFIER NULL,
+    [CategoryId] UNIQUEIDENTIFIER NULL,
+    [Tags] NVARCHAR (160) NOT NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Course_Author_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Author] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE NO ACTION
+);
+GO
+
+
+
