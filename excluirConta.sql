8739e416a56d268b3d84ece79925c050f48161bb
ValmirTucci <valmirizzo@gmail.com>
2022-11-02 23:12:00 -0300

    ExcluindoContas

---
+++ excluirConta.sql
@@ -0,0 +1,14 @@
+--Excluir a conta
+    CREATE OR ALTER PROCEDURE spDeleteStudent(
+        @studentId UNIQUEIDENTIFIER
+    )
+    AS
+        BEGIN TRANSACTION
+            DELETE FROM 
+                [StudentCourse] 
+            WHERE [StudentId] = @studentId
+
+            DELETE FROM 
+                [Student] 
+            WHERE [Id] = @studentId
+        COMMIT
---
+++ executandoProcedure.sql
@@ -0,0 +1,5 @@
+
+EXEC spDeleteStudent 'd9e0c872-93bd-457d-89a4-19028332eefc'
+
+SELECT * FROM [StudentCourse]
+SELECT * FROM [Student]
